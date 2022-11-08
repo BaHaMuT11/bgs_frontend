a0e451fbf2f3054a1ef65d114b60f7b16c18770d
Raúl Pardo Zurita <raul.pardoz@gmail.com>
2022-11-08 04:30:51 -0300

    Se finaliza implementación de obtener publicaciones
    - Se obtienen publicaciones populares con ordenamiento
    - Se obtiene catálogo de publicaciones
    - Se gestiona rating y autenticación
    - Se gestionan listas según estado de la publicación

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,9 +1,8 @@
 import React from "react"
 import {Badge, Button, Card, Col} from "react-bootstrap"
 import "./game_card.scss"
-import EldenRing from "../../assets/img/elden_ring.png"
-import GodOfWar from "../../assets/img/gordo_ragnarok.png"
 import {useNavigate} from "react-router-dom"
+import {obtenerCLP} from "../../util/clp_parser.js";
 
 const GameCard = (props) => {
 
@@ -13,49 +12,254 @@ const GameCard = (props) => {
         navigate(`/detail/${id}`)
     }
 
+    const voteOneStar = () => {
+
+    }
+
+    const voteTwoStar = () => {
+
+    }
+
+    const voteThreeStar = () => {
+
+    }
+
+    const voteFourStar = () => {
+
+    }
+
+    const voteFiveStar = () => {
+
+    }
+
+    const renderStars = (estrellas) => {
+        if (estrellas == 0 && props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteOneStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteTwoStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteThreeStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFourStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFiveStar()}
+                    ></i>
+                </>
+            )
+        }
+        if (estrellas == 1 && props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteOneStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteTwoStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteThreeStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFourStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFiveStar()}
+                    ></i>
+                </>
+            )
+        }
+        if (estrellas == 2 && props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteOneStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteTwoStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteThreeStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFourStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFiveStar()}
+                    ></i>
+                </>
+            )
+        }
+        if (estrellas == 3 && props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteOneStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteTwoStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteThreeStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFourStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFiveStar()}
+                    ></i>
+                </>
+            )
+        }
+        if (estrellas == 4 && props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteOneStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteTwoStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteThreeStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteFourStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star"
+                       onClick={ () => voteFiveStar()}
+                    ></i>
+                </>
+            )
+        }
+        if (estrellas == 5 && props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteOneStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteTwoStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteThreeStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteFourStar()}
+                    ></i> &nbsp;
+                    <i className="fa-solid fa-2x star-authenticated fa-star star-filled"
+                       onClick={ () => voteFiveStar()}
+                    ></i>
+                </>
+            )
+        }
+
+        if (estrellas == 0 && !props.estadoAutenticacion) {
+            return (
+                <p className="text-info">Sin valoraciones aun</p>
+            )
+        }
+        if (estrellas == 1 && !props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i>
+                </>
+            )
+        }
+        if (estrellas == 2 && !props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i>
+                </>
+            )
+        }
+        if (estrellas == 3 && !props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i>
+                </>
+            )
+        }
+        if (estrellas == 4 && !props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star"></i>
+                </>
+            )
+        }
+        if (estrellas == 5 && !props.estadoAutenticacion) {
+            return (
+                <>
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i> &nbsp;
+                    <i className="fa-solid fa-2x fa-star star-filled"></i>
+                </>
+            )
+        }
+    }
 
     return (
         <>
             {
-                props.rating === "high" ?
+                props.estadoAutenticacion ?
                     <Col className="col-12 col-md-6 col-lg-4 col-xl-3 py-2 game-card">
                         <Card bg={"dark"} text={"light"} className="game-box">
-                            <Card.Img variant="top" src={EldenRing} />
+                            <Card.Img variant="top" src={props.imagen} />
                             <Card.Body>
-                                <Card.Title>Elden Ring</Card.Title>
-                                <Badge bg="secondary">PC</Badge>{' '}
-                                <Badge bg="danger">Físico</Badge>{' '}
+                                <Card.Title>{props.juego}</Card.Title>
+                                <Badge bg="secondary">{props.plataforma}</Badge>{' '}
+                                <Badge bg="danger">{props.formato}</Badge>{' '}
                                 <div className="py-3 text-center">
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
+                                    {
+                                        renderStars(props.rating)
+                                    }
                                 </div>
                                 <Button className="w-100" variant="warning" onClick={
                                     ()=>{handleClick(1)}
-                                    }>$25.872 CLP</Button>
+                                }>{`${obtenerCLP(props.precio)} CLP`}</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                     :
                     <Col className="col-12 col-md-6 col-lg-4 col-xl-3 py-2 game-card">
                         <Card bg={"dark"} text={"light"} className="game-box">
-                            <Card.Img variant="top" src={GodOfWar} />
+                            <Card.Img variant="top" src={props.imagen} />
                             <Card.Body>
-                                <Card.Title>God of War: Ragnarok</Card.Title>
-                                <Badge bg="secondary">PS5</Badge>{' '}
-                                <Badge bg="danger">Digital</Badge>{' '}
+                                <Card.Title>{props.juego}</Card.Title>
+                                <Badge bg="secondary">{props.plataforma}</Badge>{' '}
+                                <Badge bg="danger">{props.formato}</Badge>{' '}
                                 <div className="py-3 text-center">
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
-                                    <i className="fa-solid fa-2x fa-star star-filled"></i>
-                                    <i className="fa-solid fa-2x fa-star"></i>
-                                    <i className="fa-solid fa-2x fa-star"></i>
-                                    <i className="fa-solid fa-2x fa-star"></i>
+                                    {
+                                        renderStars(props.rating)
+                                    }
                                 </div>
-                                <Button className="w-100" variant="warning" onClick={
-                                    ()=>{handleClick(2)}
-                                }>$80.000 CLP</Button>
+                                <div className="text-center text-white">
+                                    {`${obtenerCLP(props.precio)} CLP`}
+                                </div>
                             </Card.Body>
                         </Card>
                     </Col>
--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from "react"
 import "./products.scss"
-import {Row, Col} from "react-bootstrap";
-import Hero from "../../components/Hero/Hero.jsx";
-import GameCard from "../../components/GameCard/GameCard.jsx";
+import {Row, Col} from "react-bootstrap"
+import Hero from "../../components/Hero/Hero.jsx"
+import GameCard from "../../components/GameCard/GameCard.jsx"
+import axios from "axios"
+import {URL_OBTENER_PUBLICACIONES} from "../../services/publicaciones.js"
+import Swal from "sweetalert2"
+import {ProductContext} from "../../context/ProductProvider.jsx"
+import {UserContext} from "../../context/UserProvider.jsx"
 
 const Products = () => {
+
+    const {ntk, autenticado} = useContext(UserContext)
+    const {setPublicaciones, publicaciones} = useContext(ProductContext)
+
+    const [populares, setPopulares] = useState([])
+
+
+    const asignarPublicaciones = async () => {
+        const configsItemList = {
+            headers: {
+                "Content-Type": "Application/JSON",
+                "Authorization": "Bearer " + ntk
+            }
+        }
+
+        try {
+            const {data} = await axios.get(URL_OBTENER_PUBLICACIONES, configsItemList)
+
+            if (data.estado.codigo == "200") {
+
+                const items = data.publicaciones
+
+                const asignarPublicaciones = () => {
+                    let publis = []
+
+                    for (let publicacion of items) {
+                        if (publicacion.estado != "DESACTIVADO" && publicacion.estado != "VENDIDO") {
+                            publis = [...publis, publicacion]
+                        }
+                    }
+                    setPublicaciones(items)
+                }
+                asignarPublicaciones()
+
+                const asignarPopulares = () => {
+                    let popus = []
+
+                    items.forEach( (publicacion, index) => {
+                        if (index < 3 && (publicacion.estado != "DESACTIVADO" && publicacion.estado != "VENDIDO") ) {
+                            popus = [...popus, publicacion]
+                        }
+                    })
+                    popus.sort((a, b) => (b.rating - a.rating))
+                    setPopulares(popus)
+                }
+                asignarPopulares()
+
+            } else {
+                Swal.fire(
+                    'Whooooops',
+                    "No se pueden obtener las publicaciones",
+                    'error'
+                )
+            }
+        }
+        catch (error) {
+            Swal.fire(
+                'Whooooops',
+                "Hubo un error de conexión con el servidor",
+                'error'
+            )
+        }
+
+    }
+
+    useEffect( () => {
+        asignarPublicaciones()
+    }, [])
+
     return (
         <>
             <Row className="py-3">
@@ -14,12 +88,35 @@ const Products = () => {
             </Row>
             <Row>
                 <h2 className="display-6 text-white">Populares</h2>
-                <GameCard rating="high"  />
+                {
+                    populares.map(publicacion => (
+                        <GameCard estadoAutenticacion={autenticado}
+                                  juego={publicacion.juego}
+                                  plataforma={publicacion.plataforma}
+                                  formato={publicacion.formato}
+                                  rating={publicacion.rating}
+                                  precio={publicacion.precio}
+                                  imagen={publicacion.imagen}
+                                  key={publicacion.id}
+                        />
+                    ))
+                }
             </Row>
             <Row>
                 <h2 className="display-6 text-white">Catálogo</h2>
-                <GameCard rating="high" />
-                <GameCard rating="low"  />
+                {
+                    publicaciones.map(publicacion => (
+                        <GameCard estadoAutenticacion={autenticado}
+                                  juego={publicacion.juego}
+                                  plataforma={publicacion.plataforma}
+                                  formato={publicacion.formato}
+                                  rating={publicacion.rating}
+                                  precio={publicacion.precio}
+                                  imagen={publicacion.imagen}
+                                  key={publicacion.id}
+                        />
+                    ))
+                }
             </Row>
         </>
     )
