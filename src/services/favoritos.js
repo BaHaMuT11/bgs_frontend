466474b083ecb92c09b14b87889ede20753a7491
BaHaMuT <bahamut@bahatech.cl>
2026-01-12 01:04:43 -0300

    Se realiza modificación para nueva máscara de servicios

--- src/services/favoritos.js
+++ src/services/favoritos.js
@@ -1,3 +1,3 @@
-export const URL_AGREGAR_FAVORITO = "https://www.bahatech.cl:8443/baha_games_marketplace/publicacion/agregar_favorito"
-export const URL_OBTENER_FAVORITOS_USUARIO = "https://www.bahatech.cl:8443/baha_games_marketplace/publicacion/favoritos/"
-export const URL_ELIMINAR_FAVORITO = "https://www.bahatech.cl:8443/baha_games_marketplace/favoritos/eliminar/"
+export const URL_AGREGAR_FAVORITO = "https://www.bahatech.cl/baha_games_marketplace/publicacion/agregar_favorito"
+export const URL_OBTENER_FAVORITOS_USUARIO = "https://www.bahatech.cl/baha_games_marketplace/publicacion/favoritos/"
+export const URL_ELIMINAR_FAVORITO = "https://www.bahatech.cl/baha_games_marketplace/favoritos/eliminar/"
--- src/services/interesados.js
+++ src/services/interesados.js
@@ -1,4 +1,4 @@
-export const URL_MOSTRAR_INTERES = "https://www.bahatech.cl:8443/baha_games_marketplace/publicacion/mostrar_interes"
-export const URL_OBTENER_INTERESADOS = "https://www.bahatech.cl:8443/baha_games_marketplace/publicacion/interesados/"
-export const URL_CERRAR_VENTA = "https://www.bahatech.cl:8443/baha_games_marketplace/interesados/cerrar"
-export const URL_OBTENER_GANADOR = "https://www.bahatech.cl:8443/baha_games_marketplace/ganador/"
+export const URL_MOSTRAR_INTERES = "https://www.bahatech.cl/baha_games_marketplace/publicacion/mostrar_interes"
+export const URL_OBTENER_INTERESADOS = "https://www.bahatech.cl/baha_games_marketplace/publicacion/interesados/"
+export const URL_CERRAR_VENTA = "https://www.bahatech.cl/baha_games_marketplace/interesados/cerrar"
+export const URL_OBTENER_GANADOR = "https://www.bahatech.cl/baha_games_marketplace/ganador/"
